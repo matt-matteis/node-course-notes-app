ef2ca71b8de6a256d36a0ab590437df2441af199
Matt <matt@Lauras-MacBook.fios-router.home>
2018-07-07 16:37:51 -0400

    heroku config

--- server/server.js
+++ server/server.js
@@ -7,6 +7,7 @@ var {Todo} = require('./models/todo');
 var {User} = require('./models/user');
 
 var app = express();
+const port = process.eng.PORT || 3000;
 
 app.use(bodyParser.json());
 
@@ -36,14 +37,17 @@ app.get('/todos/:id', (req, res) => {
     return res.status(404).send();
   }
   Todo.findById(id).then((todo) => {
+    if (!todo) {
+      return res.status(404).send();
+    }
     res.send({todo});
   }, (e) => {
     res.status(400).send();
   })
 });
 
-app.listen(3000, () => {
-  console.log('Started on port 3000');
+app.listen(port, () => {
+  console.log(`Started on port ${port}`);
 });
 
 module.exports = {app};
